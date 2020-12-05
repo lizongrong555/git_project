38ca91812b1cf4b7a470eedf589038821bb96d25
lizongrong555 <74592435+lizongrong555@users.noreply.github.com>
2020-12-05 23:55:24 +0800

    Update H13.java

--- student_prc/src/com/student/model/H13.java
+++ student_prc/src/com/student/model/H13.java
@@ -5,6 +5,9 @@ class H13 {
 	public static void main(String[] args) {
 		
 		System.out.println("helloworld");
+                System.out.println("lizongrong555");
+                System.out.println("20180217086");
+
 
 	}
 
